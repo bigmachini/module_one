49808fe16ffbd43667dd6f0e886de173864937e6
bigmachini <elisha.wambiji@bigmachini.net>
2019-02-11 13:21:44 +0300

    added new bubble sort code

--- linear_search.py
+++ linear_search.py
@@ -7,4 +7,4 @@ def linear_seach(data, search_value):
 
 data = [-1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 6, 7, 7, 8, 9, 12, 14, 14, 24, 24, 24, 24, 35, 41, 43, 43, 45, 52, 54, 124, 124, 214, 323, 342, 412, 412, 421, 424, 1341, 4452]
 print('is 1000 in data {} '.format(linear_seach(data, 1000)))
-print('is -1 in data {} '.format(linear_seach(data, -1)))
+print('is 1000 in data {} '.format(linear_seach(data, 1000)))
