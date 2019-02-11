ee400538b98cd8f8549bac1ebebc5fa5d43355ec
bigmachini <elisha.wambiji@bigmachini.net>
2019-02-11 16:16:31 +0300

    binary search

--- bubble_sort.py
+++ bubble_sort.py
@@ -2,13 +2,13 @@ def bubble_sort(data):
     size = len(data)
     swap = False
     for i in range(size-1):
-        if data[i] > data[i+1]:
+        if data[i] < data[i+1]:
             swap = True
             data[i],data[i+1] = data[i+1],data[i]
     while swap:
         swap = False
         for i in range(size-1):
-            if data[i] > data[i+1]:
+            if data[i] < data[i+1]:
                 swap = True
                 data[i],data[i+1] = data[i+1],data[i]
 
@@ -17,7 +17,7 @@ def bubble_sort_two(data):
     list_size = len(data)
     for i in range(list_size,0, -1):
         for j in range(i-1):
-            if data[j] < data[j+1]:
+            if data[j] > data[j+1]:
                 data[j],data[j+1] = data[j+1],data[j]
 
 
